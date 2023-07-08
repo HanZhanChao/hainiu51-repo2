ffe734afb312490cad053864b42aa70e535433fc
hainiu <hainiu@example.com>
2023-07-08 18:03:30 +0800

    修改Demo01

--- day22-mvn02/src/main/java/Demo01.java
+++ day22-mvn02/src/main/java/Demo01.java
@@ -6,5 +6,9 @@ public class Demo01 {
 
     public static void main(String[] args) {
         System.out.println(111);
+
+        System.out.println(333);
+        System.out.println(444);
+
     }
 }
